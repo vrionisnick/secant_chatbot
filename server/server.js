71786c99a0cae605bcc7b9fa85a621577da577b2
vrionisnick <nvrionis@infolysis.gr>
2024-03-14 15:54:54 +0200

    Remove consolelog

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 require('dotenv').config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
